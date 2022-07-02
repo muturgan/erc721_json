abe2353efd5957b9b1a07575a62f6274c0f435b3
Andrey Sakharov <sakharov.a@n-t.io>
2022-07-02 15:36:07 +0300

    nft contracts were added

---
+++ contracts/Base64Json.sol
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+import '@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol';
+import 'base64-sol/base64.sol';
+
+
+contract Base64Json is ERC721Enumerable
+{
+	struct Card {
+		string beast;
+		string action;
+		string rare;
+	}
+
+	mapping(uint256 => Card) private tokenToCard;
+	uint256 private tokenIdCounter;
+
+	constructor() ERC721('Base64Json', 'BJ') {}
+
+
+	function mint(string calldata _beast, string calldata _action, string calldata _rare) external {
+		Card memory card = Card(_beast, _action, _rare);
+		unchecked {
+			tokenIdCounter += 1;
+		}
+		uint256 tokenId = tokenIdCounter;
+		tokenToCard[tokenId] = card;
+		_safeMint(msg.sender, tokenId);
+	}
+
+	function tokenURI(uint256 tokenId) public view override returns(string memory) {
+		_requireMinted(tokenId);
+
+		Card memory card = tokenToCard[tokenId];
+		string memory json = string.concat(
+			'{"name":"PureJson","description":"practicing to return a base64 encoded json string from the tokenURI method call","external_url":"https://austingriffith.com/portfolio/paintings/","image": "https://austingriffith.com/images/paintings/',
+			card.beast,
+			'.jpg","attributes":[{"trait_type":"Beast","value":"',
+			card.beast,
+			'"},{"trait_type":"Action"","value":"',
+			card.action,
+			'"},{"trait_type":"Rare","value":"',
+			card.rare,
+			'"}]}'
+		);
+
+		return string.concat(
+			'data:application/json;base64,',
+			Base64.encode(
+				bytes(json)
+			)
+		);
+	}
+}
